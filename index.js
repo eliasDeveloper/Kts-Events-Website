e1d33c6cdfa4c7a87a8920d5df9e92bdb3317e06
AzureAD\FadyChebly <fady.c@sirenassociates.com>
2022-03-31 23:52:22 +0300

    mecha el hal

--- index.js
+++ index.js
@@ -11,14 +11,10 @@ const jwt = require('jsonwebtoken')
 const verify = require('./middleware/verifyToken')
 const cookieParser = require('cookie-parser')
 const nodemailer = require('nodemailer')
-<<<<<<< HEAD
 const adminRoute = require('./routes/kts-admin')
-=======
-const fs = require('fs')
-const csv = require('csv-parser')
->>>>>>> develop
 require('dotenv').config()
 
+//mecha el hal?
 // database connection conf
 mongoose.connect(process.env.CONNECTION_STRING, {
 	useNewUrlParser: true,
@@ -64,61 +60,29 @@ app.get('/contact', (req, res) => {
 	res.render('Landing-Pages/contact', { title: "Contact Us" })
 })
 
-app.get('/register', (req, res) => {
-	res.render('Landing-Pages/register', { layout: "./layouts/register-layout", title: "Register" })
-})
-
 app.get('/login', (req, res) => {
 	res.render('Landing-Pages/login', { layout: "./layouts/login-layout", title: "Login" })
 })
-<<<<<<< HEAD
-=======
-app.get('/password-grant', (req, res) => {
-	res.render('Landing-Pages/password-grant', { layout: "./layouts/password-grant", title: "Password Grant" })
-})
-app.get('/welcome', (req, res) => {
-	if (req.cookies.token)
-		res.render('Landing-Pages/welcome', { layout: "./layouts/welcome-layout", title: "Welcome!!" })
-	else {
-		res.redirect('login')
+
+app.post('/login', async (req, res) => {
+	res.clearCookie("token");
+	const { error } = schema.validate(req.body)
+	if (error) {
+		return res.status(400).send(error.details[0].message)
 	}
-})
-//end of landing pages routing
-
-
-//start of admin routes
-app.get('/kts-admin/home', async (req, res) => {
-	const events = await Event.find({})
-	res.render('Kts-Admin/home', { events, layout: "./layouts/admin-layout", title: "Admin - Home" })
-})
-
-//start of add event
-//start of event owner add page
-app.get('/kts-admin/new-event', (req, res) => {
-	res.render('Kts-Admin/event-owner', { layout: "./layouts/admin-layout", title: "Admin - New Event" })
-})
-
-//post the owner for a new event and go to fill the event with the needed data
-app.post('/kts-admin/event', async (req, res) => {
-	const { email } = req.body
-	const newEvent = new Event({ owner: `${email}` })
-	await newEvent.save().then(res => { console.log(`success to post event owner`) }).catch(err => { console.log(err) })
-	const id = newEvent._id.toString()
-	res.redirect(`/kts-admin/event/${id}`)
-})
-//end of add event owner logic to an event
-
-//start of add needed data and package for a specific event
-app.get('/kts-admin/event/:id', async (req, res) => {
-	const { id } = req.params
-	let event = await Event.findById(id).then(res => { console.log(`success to reach the new event with id: ${id} ${res}`) }).catch(err => { console.log(err) })
-	while (event == undefined) {
-		event = await Event.findById(id)
-		console.log('in the loop')
+	const { email, password } = req.body
+	const user = await User.findOne({ email })
+	if (!user) {
+		return res.status(400).send('failed login: invalid credentials')
 	}
-	res.render('Kts-Admin/event', { layout: "./layouts/admin-layout", title: "Event", event })
+	const validPass = await bcrypt.compare(password, user.password)
+	if (!validPass) {
+		return res.status(400).send('failed login: invalid credentials')
+	}
+	const token = jwt.sign({ user }, process.env.TOKEN_SECRET_KEY, { expiresIn: '24h' })
+	res.cookie('token', token.toString())
+	res.redirect('/welcome')
 })
->>>>>>> develop
 
 app.get('/welcome', (req, res) => {
 	if (req.cookies.token)
@@ -158,25 +122,7 @@ app.post('/api/user/register', async (req, res) => {
 	}
 })
 
-app.post('/login', async (req, res) => {
-	res.clearCookie("token");
-	const { error } = schema.validate(req.body)
-	if (error) {
-		return res.status(400).send(error.details[0].message)
-	}
-	const { email, password } = req.body
-	const user = await User.findOne({ email })
-	if (!user) {
-		return res.status(400).send('failed login: invalid credentials')
-	}
-	const validPass = await bcrypt.compare(password, user.password)
-	if (!validPass) {
-		return res.status(400).send('failed login: invalid credentials')
-	}
-	const token = jwt.sign({ user }, process.env.TOKEN_SECRET_KEY, { expiresIn: '24h' })
-	res.cookie('token', token.toString())
-	res.redirect('/welcome')
-})
+
 app.post('/password-grant', async (req, res) => {
 	let emails = []
 	let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -210,7 +156,7 @@ app.post('/password-grant', async (req, res) => {
 					console.log('email already exists')
 				}
 				//Create a new User
-				else{
+				else {
 					transporter.sendMail(mailOptions, (err) => {
 						if (err) {
 							console.log(err);
@@ -220,20 +166,21 @@ app.post('/password-grant', async (req, res) => {
 						}
 					});
 					const user = new User({
-					name: 'test',
-					email: emails[i],
-					password: password,
-					isAdmin: 0
-				})
-				try {
-					user.save()
-				} catch (err) {
-					console.log(err)
+						name: 'test',
+						email: emails[i],
+						password: password,
+						isAdmin: 0
+					})
+					try {
+						user.save()
+					} catch (err) {
+						console.log(err)
+					}
 				}
 			}
-		}
-	})
+		})
 })
+
 app.post('/logout', (req, res) => {
 	res.clearCookie("token");
 	res.redirect('/login')
